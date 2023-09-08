16e05024306b2eb67139d17ed721a2ade26d0d81
Fatou01 <sfatou@ept.sn>
2023-09-08 21:36:06 +0000

    page_accueil/page_admin/list_users

--- WeBib/views.py
+++ WeBib/views.py
@@ -1,9 +1,11 @@
 
 # Create your views here.
+import base64
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate,login
 from django.contrib.auth.forms import AuthenticationForm
 from .models import *
+import requests
 
 
 def view_accueil_user(request):
@@ -13,14 +15,19 @@ def view_accueil_admin(request):
     return render(request,"page_accueil_admin.html")
 
 def view_liste_utilisateurs(request):
+    # Remplacez "YOUR_USERNAME" et "YOUR_PASSWORD" par vos informations d'authentification
+    username = "admin"
+    password = "mars"
+    
+    # authentification http request
+    headers = {
+        "Authorization": f"Basic {base64.b64encode(f'{username}:{password}'.encode()).decode()}"
+    }
   
-    #utilisateurs = Utilisateur.objects.all() # Récupérez tous les utilisateurs de la base de données récupérés à partir de l'inscription
-     # Ceci est un text d'utilisateurs; à enlever après
-    utilisateurs = [
-        Utilisateur(first_name="Doe", last_name="John", username="motdepasse1",email="email1"),
-        Utilisateur(first_name="Gueye", last_name="John", username="motdepasse2",email="email2"),
-        Utilisateur(first_name="Sall", last_name="John", username="motdepasse3",email="email3"),
-    ]
+    api_url = "https://127.0.0.1:8000/users/"
+    response = requests.get(api_url,headers=headers)
+    utilisateurs = response.json()  # Si l'API renvoie des données JSON
+
     return render(request, 'page_list_users.html', {'utilisateurs': utilisateurs})
   
 def view_nombre_visiteurs(request):

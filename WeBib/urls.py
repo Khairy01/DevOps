7f52b9b211a3d9131f2e0b358c3e758c46865513
Fadima18 <dnfatou@ept.sn>
2023-09-16 02:28:15 +0000

    pipeline4

--- WeBib/urls.py
+++ WeBib/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import view_liste_utilisateurs, home_view, view_accueil_admin
-from django.conf.urls.static import static
 
 urlpatterns = [
     path("list_users/", view_liste_utilisateurs, name="list_users"),
